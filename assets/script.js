028ae831aa3a9f35033eec88891df0cbc5343825
omercanbalandi <balandiomer@gmail.com>
2018-09-06 01:15:18 +0300

    up #2

--- assets/script.js
+++ assets/script.js
@@ -14,12 +14,16 @@ for (let i in tags) {
 }
 
 for (let i in projects) {
+
     projects_.innerHTML += `
     <div class="c_e">
         <span class="${tags[projects[i].cat].name}">${projects[i].name}</span>
         <div class="a_b">
             <a href="${projects[i].source}" target="_blank">Source</a>
             <a href="${projects[i].live}" target="_blank">Live</a>
+        </div>
+        <div class="a_d">
+            <span>${projects[i].tags.join("</span><span>")}</span>
         </div>
     </div>
     `;
