97e1b4c77bf93f72e26ff1ebb0f8d6bcbda1e018
omercanbalandi <balandiomer@gmail.com>
2018-09-14 21:37:15 +0300

    added "circles"

--- assets/data.js
+++ assets/data.js
@@ -10,5 +10,6 @@ const projects = [
     {cat: "p5", tags: ["javascript", "p5js", "matrix multiplication"], name: "3D Projection", source:"https://github.com/obsfx/3DRotation-p5js", live: "https://obsfx.github.io/3dprojection"},
     {cat: "gd", tags: ["javascript", "p5js", "ludum dare 42", "game development"], name: "backSpace-x", source:"https://github.com/obsfx/ld42-backspace-x", live: "https://obsfx.github.io/backspacex"},
     {cat: "gd", tags: ["javascript", "game development"], name: "spaceinvaders.js", source:"https://github.com/obsfx/spaceinvaders.js", live: "https://obsfx.github.io/spaceinvadersjs"},
-    {cat: "cc", tags: ["javascript", "p5js", "creative coding"], name: "deep", source:"https://github.com/obsfx/creativecoding-practices-with-p5js/tree/master/1_deep", live: "https://obsfx.github.io/deep"}
+    {cat: "cc", tags: ["javascript", "p5js", "creative coding"], name: "deep", source:"https://github.com/obsfx/creativecoding-practices-with-p5js/tree/master/1_deep", live: "https://obsfx.github.io/deep"},
+    {cat: "cc", tags: ["javascript", "p5js", "creative coding"], name: "circles", source:"https://github.com/obsfx/creativecoding-practices-with-p5js/tree/master/2_circles", live: "https://obsfx.github.io/circles"}
 ]
