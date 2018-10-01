859589d22641e138bf14ec718e02b43ff73a07b0
omercanbalandi <balandiomer@gmail.com>
2018-10-01 23:16:59 +0300

    tetrisjs

--- assets/data.js
+++ assets/data.js
@@ -12,5 +12,6 @@ const projects = [
     {cat: "gd", tags: ["javascript", "game development"], name: "spaceinvaders.js", source:"https://github.com/obsfx/spaceinvaders.js", live: "https://obsfx.github.io/spaceinvadersjs"},
     {cat: "cc", tags: ["javascript", "p5js", "creative coding"], name: "deep", source:"https://github.com/obsfx/creativecoding-practices-with-p5js/tree/master/1_deep", live: "https://obsfx.github.io/deep"},
     {cat: "cc", tags: ["javascript", "p5js", "creative coding"], name: "circles", source:"https://github.com/obsfx/creativecoding-practices-with-p5js/tree/master/2_circles", live: "https://obsfx.github.io/circles"},
-    {cat: "cc", tags: ["javascript", "p5js", "creative coding"], name: "waves", source:"https://github.com/obsfx/creativecoding-practices-with-p5js/tree/master/3_waves", live: "https://obsfx.github.io/waves"}
+    {cat: "cc", tags: ["javascript", "p5js", "creative coding"], name: "waves", source:"https://github.com/obsfx/creativecoding-practices-with-p5js/tree/master/3_waves", live: "https://obsfx.github.io/waves"},
+    {cat: "gd", tags: ["javascript", "p5js", "game development"], name: "tetris.js", source:"https://github.com/obsfx/tetris-js", live: "https://obsfx.github.io/tetrisjs"}
 ]
