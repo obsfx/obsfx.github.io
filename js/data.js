5a5d3643368cfb098743efb18af61fdf559655bf
omercanbalandi <balandiomer@gmail.com>
2019-01-19 20:24:35 +0300

    v3

---
+++ js/data.js
@@ -0,0 +1,62 @@
+const projects = [
+    {
+        tags: ["javascript", "game development"], 
+        name: "spaceinvaders.js", 
+        desc: "my first game development experiment which i created without any game development framework. i only used canvas 2d api.",
+        source: "https://github.com/obsfx/spaceinvaders.js", 
+        live: "https://obsfx.github.io/spaceinvadersjs",
+        img: "si.gif"
+    },
+
+    {
+        tags: ["javascript", "p5.js", "ludum dare 42", "game development"], 
+        desc: "i created this game and all game content during ludum dare 42 game jam in 48 hours with p5.js library.",
+        name: "backSpace-x", 
+        source: "https://github.com/obsfx/ld42-backspace-x", 
+        live: "https://obsfx.github.io/backspacex",
+        img: "bx.jpg"
+    },
+
+    {
+        tags: ["javascript", "game development"], 
+        name: "tetris.js", 
+        desc: "a tetris clone which i created with only vanilla javascript.",
+        source: "https://github.com/obsfx/tetris-js", 
+        live: "https://obsfx.github.io/tetrisjs",
+        img: "tetris.gif"
+    },
+
+    {
+        tags: ["javascript", "p5.js", "matrix multiplication"], 
+        name: "3D Projection", 
+        desc : "javascript/p5js version of daniel shiffman's 3D projection coding challenge.",
+        source: "https://github.com/obsfx/3DRotation-p5js", 
+        live: "https://obsfx.github.io/3dprojection",
+        img: "3d.gif"
+    },
+
+    {
+        tags: ["javascript", "p5js", "creative coding"], 
+        name: "creative coding expirements with p5.js", 
+        desc: "i am trying to learn fundamentals of creative coding and trying to make simple animations with p5.js library. you can find that animations in this github repo.",
+        source: "https://github.com/obsfx/creativecoding-practices-with-p5js/",
+        img: "cc.gif"
+    },
+
+    {
+        tags: ["javascript", "phaser", "game development", "ludum dare 43"], 
+        name: "purgatory", 
+        desc: "i made this game and all game content during ludum dare 43 game jam for compo mode with phaser library.",
+        source: "https://github.com/obsfx/ld43-purgatory", 
+        live: "https://obsfx.github.io/purgatory",
+        img: "pg.jpg"
+    },
+
+    {
+        tags: ["javascript", "vuejs", "front-end development"], 
+        name: "vuejs practices", 
+        desc: "some small web apps which i created to learn basics of vuejs.",
+        source: "https://github.com/obsfx/vuejs-practices", 
+        img: "vue.gif"
+    },
+]
---
+++ js/script.js
@@ -0,0 +1,55 @@
+const projects_ = document.getElementById("expcon");
+
+for (let k in projects) {
+    let i = projects.length - 1 - k;
+
+    let sourcelink = "";
+    let livelink = "";
+
+    if (projects[i].source) {
+        sourcelink = `<a href="${projects[i].source}" target="_blank">source code</a>`;
+    }
+
+    if (projects[i].live) {
+        livelink = `<a href="${projects[i].live}" target="_blank">live demo</a>`;
+    }
+
+    projects_.innerHTML += `
+        <div class="exp-items-item">
+            <div class="exp-items-item-showcase">
+                <img src="./assets/${projects[i].img}">
+            </div>
+
+            <div class="exp-items-item-head">
+                <h2>${projects[i].name}</h2>
+                <p>${projects[i].desc}</p>
+            </div>
+
+            <div class="exp-items-item-bottom">
+                <h5>related topics</h5>
+
+                <div class="exp-items-item-bottom-tags">
+                    <span>${projects[i].tags.join("</span><span>")}</span>
+                </div>
+            </div>
+
+            <div class="exp-items-item-links">
+                ${sourcelink}
+                ${livelink}
+            </div>
+        </div>
+    `;
+
+    /*projects_.innerHTML += `
+    <div class="ex">
+        <span class="${tags[projects[i].cat].name}">${projects[i].name}</span>
+        <div class="item_inline">
+            <a href="${projects[i].source}" target="_blank">Source</a>
+            <a href="${projects[i].live}" target="_blank">Live</a>
+        </div>
+        <div class="item_tags">
+            <span>${projects[i].tags.join("</span><span>")}</span>
+        </div>
+    </div>
+    `;*/
+}
