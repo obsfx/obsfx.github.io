d84deb4f76ca290cec3d3500539f1cc7462d6d0e
Omercan Balandi <balandiomer@gmail.com>
2022-02-12 03:28:47 +0300

    Updates

---
+++ _app/pages/__layout.svelte-9656d9ae.js
@@ -0,0 +1,49 @@
+import{S as h,i as $,s as b,F as w,e as p,k as E,t as v,c,a as u,d,m as S,h as j,g as _,G as y,H as q,I as z,J as F,q as I,o as O}from"../chunks/vendor-6fa78954.js";/* empty css                    */function T(s){let t,l,a,o,r,f;const m=s[1].default,e=w(m,s,s[0],null);return{c(){t=p("main"),e&&e.c(),l=E(),a=p("footer"),o=p("style"),r=v(`main {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      padding: 1rem;
+      width: 100%;
+      max-width: 600px;
+      margin: 0 auto;
+      box-sizing: border-box;
+      margin-top: 2rem;
+    }
+
+    footer {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 40px;
+    }
+
+    @media (min-width: 480px) {
+      footer {
+        padding: 40px 0;
+      }
+    }`)},l(n){t=c(n,"MAIN",{});var i=u(t);e&&e.l(i),i.forEach(d),l=S(n),a=c(n,"FOOTER",{});var x=u(a);o=c(x,"STYLE",{});var g=u(o);r=j(g,`main {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      padding: 1rem;
+      width: 100%;
+      max-width: 600px;
+      margin: 0 auto;
+      box-sizing: border-box;
+      margin-top: 2rem;
+    }
+
+    footer {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 40px;
+    }
+
+    @media (min-width: 480px) {
+      footer {
+        padding: 40px 0;
+      }
+    }`),g.forEach(d),x.forEach(d)},m(n,i){_(n,t,i),e&&e.m(t,null),_(n,l,i),_(n,a,i),y(a,o),y(o,r),f=!0},p(n,[i]){e&&e.p&&(!f||i&1)&&q(e,m,n,n[0],f?F(m,n[0],i,null):z(n[0]),null)},i(n){f||(I(e,n),f=!0)},o(n){O(e,n),f=!1},d(n){n&&d(t),e&&e.d(n),n&&d(l),n&&d(a)}}}function k(s,t,l){let{$$slots:a={},$$scope:o}=t;return s.$$set=r=>{"$$scope"in r&&l(0,o=r.$$scope)},[o,a]}class G extends h{constructor(t){super();$(this,t,k,T,b,{})}}export{G as default};
