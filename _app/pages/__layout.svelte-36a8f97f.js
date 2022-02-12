1b81cb858e8a41b90ae7452a2118e058af7aee28
Omercan Balandi <balandiomer@gmail.com>
2022-02-12 03:37:40 +0300

    Updates

---
+++ _app/pages/__layout.svelte-36a8f97f.js
@@ -0,0 +1,51 @@
+import{S as y,i as b,s as $,F as w,e as p,k as E,t as v,c,a as u,d,m as S,h as j,b as q,g as _,G as g,H as z,I as F,J as I,q as O,o as T}from"../chunks/vendor-6fa78954.js";function k(o){let t,l,a,s,r,f;const m=o[1].default,e=w(m,o,o[0],null);return{c(){t=p("main"),e&&e.c(),l=E(),a=p("footer"),s=p("style"),r=v(`@import '../app.scss';
+    main {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      padding: 1rem;
+      width: 100%;
+      max-width: 600px;
+      margin: 0 auto;
+      box-sizing: border-box;
+      margin-top: 2rem;
+    }
+
+    footer {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 40px;
+    }
+
+    @media (min-width: 480px) {
+      footer {
+        padding: 40px 0;
+      }
+    }`),this.h()},l(n){t=c(n,"MAIN",{});var i=u(t);e&&e.l(i),i.forEach(d),l=S(n),a=c(n,"FOOTER",{});var x=u(a);s=c(x,"STYLE",{css:!0});var h=u(s);r=j(h,`@import '../app.scss';
+    main {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      padding: 1rem;
+      width: 100%;
+      max-width: 600px;
+      margin: 0 auto;
+      box-sizing: border-box;
+      margin-top: 2rem;
+    }
+
+    footer {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 40px;
+    }
+
+    @media (min-width: 480px) {
+      footer {
+        padding: 40px 0;
+      }
+    }`),h.forEach(d),x.forEach(d),this.h()},h(){q(s,"css","scss")},m(n,i){_(n,t,i),e&&e.m(t,null),_(n,l,i),_(n,a,i),g(a,s),g(s,r),f=!0},p(n,[i]){e&&e.p&&(!f||i&1)&&z(e,m,n,n[0],f?I(m,n[0],i,null):F(n[0]),null)},i(n){f||(O(e,n),f=!0)},o(n){T(e,n),f=!1},d(n){n&&d(t),e&&e.d(n),n&&d(l),n&&d(a)}}}function A(o,t,l){let{$$slots:a={},$$scope:s}=t;return o.$$set=r=>{"$$scope"in r&&l(0,s=r.$$scope)},[s,a]}class G extends y{constructor(t){super();b(this,t,A,k,$,{})}}export{G as default};
