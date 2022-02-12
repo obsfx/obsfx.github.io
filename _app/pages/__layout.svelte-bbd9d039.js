89a2957de1f81ff0b01b0f90b32c9d20bb33f568
Omercan Balandi <balandiomer@gmail.com>
2022-02-12 03:57:16 +0300

    Updates

---
+++ _app/pages/__layout.svelte-bbd9d039.js
@@ -0,0 +1,49 @@
+import{S as y,i as b,s as $,F as w,e as c,k as v,t as E,c as p,a as u,d,m as S,h as j,b as q,g as _,G as g,H as z,I as F,J as I,q as O,o as T}from"../chunks/vendor-6fa78954.js";function k(o){let t,l,s,a,r,f;const m=o[1].default,e=w(m,o,o[0],null);return{c(){t=c("main"),e&&e.c(),l=v(),s=c("footer"),a=c("style"),r=E(`main {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      padding: 1rem;
+      width: 100%;
+      max-width: 600px;
+      margin: 0 auto;
+      box-sizing: border-box;
+      margin-top: 2rem;
+    }
+
+    footer {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 40px;
+    }
+
+    @media (min-width: 480px) {
+      footer {
+        padding: 40px 0;
+      }
+    }`),this.h()},l(n){t=p(n,"MAIN",{});var i=u(t);e&&e.l(i),i.forEach(d),l=S(n),s=p(n,"FOOTER",{});var x=u(s);a=p(x,"STYLE",{css:!0});var h=u(a);r=j(h,`main {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      padding: 1rem;
+      width: 100%;
+      max-width: 600px;
+      margin: 0 auto;
+      box-sizing: border-box;
+      margin-top: 2rem;
+    }
+
+    footer {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 40px;
+    }
+
+    @media (min-width: 480px) {
+      footer {
+        padding: 40px 0;
+      }
+    }`),h.forEach(d),x.forEach(d),this.h()},h(){q(a,"css","scss")},m(n,i){_(n,t,i),e&&e.m(t,null),_(n,l,i),_(n,s,i),g(s,a),g(a,r),f=!0},p(n,[i]){e&&e.p&&(!f||i&1)&&z(e,m,n,n[0],f?I(m,n[0],i,null):F(n[0]),null)},i(n){f||(O(e,n),f=!0)},o(n){T(e,n),f=!1},d(n){n&&d(t),e&&e.d(n),n&&d(l),n&&d(s)}}}function A(o,t,l){let{$$slots:s={},$$scope:a}=t;return o.$$set=r=>{"$$scope"in r&&l(0,a=r.$$scope)},[a,s]}class G extends y{constructor(t){super();b(this,t,A,k,$,{})}}export{G as default};
