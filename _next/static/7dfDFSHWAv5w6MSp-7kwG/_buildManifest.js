4af93d5a337cf1f67067e60e7b7ee425fd7fc300
obsfx <balandiomer@gmail.com>
2022-06-24 02:26:53 +0000

    Deploying to gh-pages from @ obsfx/obsfx.github.io@ffbae612bb5ce583270588678b17b83f5546251d 🚀

---
+++ _next/static/7dfDFSHWAv5w6MSp-7kwG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/318-0bc0b941f22c143e.js","static/chunks/pages/index-dad69fbb76467a65.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
