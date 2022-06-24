56eefb6484b755dc69b0e2c5619bd1461d26c997
Omercan Balandi <balandiomer@gmail.com>
2022-06-24 05:12:43 +0300

    Move head scripts

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NextHead from "next/head";
-import Script from "next/script";
 
 const Head: React.FC<{
   title: string;
@@ -11,26 +10,6 @@ const Head: React.FC<{
       <title>{title}</title>
       <meta name="description" content={description} />
       <link rel="icon" href="/static/favicon.png" />
-      {/* Global site tag (gtag.js) - Google Analytics*/}
-
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-10B5E0EP0C"
-        strategy="afterInteractive"
-      />
-
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-10B5E0EP0C');
-      `}
-      </Script>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
-        rel="stylesheet"
-      />
     </NextHead>
   );
 };
---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Global site tag (gtag.js) - Google Analytics*/}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-10B5E0EP0C"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-10B5E0EP0C');
+      `}
+        </Script>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
