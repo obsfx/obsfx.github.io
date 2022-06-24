386c18e67bd63fb1a9b09d7ac7f75f84e68ac507
Omercan Balandi <balandiomer@gmail.com>
2022-06-24 03:19:58 +0300

    Updates

---
+++ _next/static/6cCCDsdlIZK0jPQMa_lN3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/977-08a158786e592fef.js","static/chunks/pages/index-7fe2b4a5183bd8bc.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
