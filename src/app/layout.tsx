242b62891983c2cfbe249aca83746f79e72c1916
omercanbalandi <balandiomer@gmail.com>
2024-02-11 06:24:00 +0300

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <Script
-        id='google-analytics'
+        id='tagmanager-ga4'
         dangerouslySetInnerHTML={{
           __html: `
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -29,6 +29,18 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KJ8JVDFN');
+          `,
+        }}
+      />
+      <Script async src='https://www.googletagmanager.com/gtag/js?id=G-10B5E0EP0C' />
+      <Script
+        id='analytics'
+        dangerouslySetInnerHTML={{
+          __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-10B5E0EP0C');
           `,
         }}
       />
