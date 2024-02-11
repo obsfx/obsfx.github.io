aeb4e212948739f3ade9db896bfae5fe2e096a41
omercanbalandi <balandiomer@gmail.com>
2024-02-11 08:39:57 +0300

    Add static output

---
+++ out/_next/static/chunks/app/not-found-aee3eac85ed10b4e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{5368:function(n,e,u){Promise.resolve().then(u.t.bind(u,5250,23))}},function(n){n.O(0,[250,971,69,744],function(){return n(n.s=5368)}),_N_E=n.O()}]);
